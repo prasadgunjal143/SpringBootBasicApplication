f360b047dd59af1e11e22a47539f00bf6a662e3d
prasad311 <prasadgunjal311@gmail.com>
2021-11-26 15:23:50 +0530

    Initial commit

--- SimpleProject/src/com/csi/dfd/NewProject.java
+++ SimpleProject/src/com/csi/dfd/NewProject.java
@@ -2,6 +2,7 @@ package com.csi.dfd;
 
 public class NewProject {
 public static void main(String[] args) {
-	System.out.println("Hello Mates");
+	System.out.println("Hello Mates");//Changes Made By Prasad
+	System.out.println("MAMAMAM");
 }
 }
